f476ba9d2a345a25196725f82c306c6bdb45ac3c
Ying Wang <wangying@google.com>
2013-08-20 12:41:34 -0700

    Fix master apps build.
    Change-Id: Id4edfb2ccfad9d2dc8dbafdf6c15640242717468

--- uiautomator/Android.mk
+++ uiautomator/Android.mk
@@ -17,4 +17,9 @@
 # don't build uiautomator in unbundled env
 ifndef TARGET_BUILD_APPS
 include $(call all-subdir-makefiles)
+else
+ifneq ($(filter uiautomator,$(TARGET_BUILD_APPS)),)
+# used by the platform apps build.
+include $(call all-subdir-makefiles)
+endif
 endif
