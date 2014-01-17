3f0386ae0946a81747cb14cb86b74bcfcbc249a0
Brett Chabot <brettchabot@google.com>
2014-01-16 18:27:31 -0800

    Fix build - correct bad merge
    Change-Id: Iacfe397c4b14dbb6b4d7b48711d0550ca3db5663

--- support/src/android/support/test/internal/runner/TestLoader.java
+++ support/src/android/support/test/internal/runner/TestLoader.java
@@ -168,11 +168,6 @@ public class TestLoader {
             Log.w(LOG_TAG, String.format("%s in isTestClass for %s", e.toString(),
                     loadedClass.getName()));
             return false;
-        } catch (Error e) {
-            // defensively catch Errors too
-            Log.w(LOG_TAG, String.format("%s in isTestClass for %s", e.toString(),
-                    loadedClass.getName()));
-            return false;
-        }
+        } 
     }
 }
