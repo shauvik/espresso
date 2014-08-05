fec1bfec0f1ece54aabaaf6320e5087e685cf2f9
Nick Korostelev <nkorsote@google.com>
2014-08-05 11:27:35 -0700

    Added Gradle support to ASTL
    Depends on CL: https://googleplex-android-review.git.corp.google.com/#/c/517689/
    
    TODO:
    Add gradle support for:
    <meta-data android:name="listener" android:value="android.support.test.internal.runner.listener.ManifestListener" />
    
    Change-Id: I66e9c5a3bf110daeeeee8eada3a0a423517dbcbe

--- support/Android.mk
+++ support/Android.mk
@@ -1,3 +1,4 @@
+#
 # Copyright (C) 2012 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,84 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-#
 
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-
-LOCAL_MODULE := android-support-test
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_SDK_VERSION := 8
-LOCAL_STATIC_JAVA_LIBRARIES := junit4-target
-
-include $(BUILD_STATIC_JAVA_LIBRARY)
-
-# ------------------------------------------------
-# build a version without bundled dependencies
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-
-LOCAL_MODULE := android-support-test-nodeps
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_SDK_VERSION := 8
-LOCAL_JAVA_LIBRARIES := junit4-target
-
-include $(BUILD_STATIC_JAVA_LIBRARY)
-
-# -----------------------------------------------
-# build a droiddoc package for integration in d.android.com 
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := $(call all-java-files-under, src) \
-  $(call junit4_to_document, ../../../external/junit/src/org) \
-  $(call all-java-files-under, ../../../external/hamcrest/src)
-
-LOCAL_MODULE := android-support-test-docs
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_JAVA_LIBRARIES := android-support-test
-LOCAL_SDK_VERSION := 8
-LOCAL_IS_HOST_MODULE := false
-LOCAL_DROIDDOC_CUSTOM_TEMPLATE_DIR := build/tools/droiddoc/templates-sdk
-
-LOCAL_DROIDDOC_OPTIONS := \
-        -hdf android.whichdoc online \
-        -hdf template.showLanguageMenu true
-
-include $(BUILD_DROIDDOC)
-
-# ----------------------------------------------
-# build a offline droiddoc package
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := $(call all-java-files-under, src) \
-  $(call all-java-files-under, ../../../external/junit/src/org) \
-  $(call all-java-files-under, ../../../external/hamcrest/src)
-
-LOCAL_MODULE := android-support-test-offline-docs
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_JAVA_LIBRARIES := android-support-test
-LOCAL_SDK_VERSION := 8
-LOCAL_IS_HOST_MODULE := false
-LOCAL_DROIDDOC_CUSTOM_TEMPLATE_DIR := build/tools/droiddoc/templates-sdk
-
-LOCAL_DROIDDOC_OPTIONS := \
-        -offlinemode \
-        -hdf android.whichdoc offline \
-        -hdf template.showLanguageMenu true
-
-include $(BUILD_DROIDDOC)
-
-# ----------------------------------------------
-
-# Use the following include to make our test apk.
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
+include $(call all-subdir-makefiles)
