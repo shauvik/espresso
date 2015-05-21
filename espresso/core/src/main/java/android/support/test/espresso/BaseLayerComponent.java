71ee99c7dccab5060bbe9533cdda0ace06754219
Valera Zakharov <valeraz@google.com>
2015-05-21 14:25:45 -0700

    Sync dagger modules with internal codebase
    Change-Id: I1e2fc7a069f1c50a7f27ec609d4bb6629375d84d

--- espresso/core/src/main/java/android/support/test/espresso/BaseLayerComponent.java
+++ espresso/core/src/main/java/android/support/test/espresso/BaseLayerComponent.java
@@ -3,6 +3,7 @@ package android.support.test.espresso;
 import android.support.test.espresso.base.ActiveRootLister;
 import android.support.test.espresso.base.BaseLayerModule;
 import android.support.test.espresso.base.IdlingResourceRegistry;
+import android.support.test.espresso.base.UiControllerModule;
 
 import dagger.Component;
 
@@ -11,7 +12,7 @@ import javax.inject.Singleton;
 /**
  * Dagger component for base classes.
  */
-@Component(modules = BaseLayerModule.class)
+@Component(modules = {BaseLayerModule.class, UiControllerModule.class})
 @Singleton
 public interface BaseLayerComponent {
   BaseLayerModule.FailureHandlerHolder failureHolder();
